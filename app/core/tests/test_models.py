e529f5d3da8c1e2a650bba58b7051eb87beb98c4
pmakaruk <makar1998@yahoo.pl>
2020-10-27 21:00:44 +0100

    Added Recipe model.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -62,3 +62,14 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Potato soup',
+            time_minutes=5,
+            price=3.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
